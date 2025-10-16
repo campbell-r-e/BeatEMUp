9794bb453ccf871e7dce98d15fa3f622de0eaf88
bsucs <122378640+bsucs@users.noreply.github.com>
2025-10-15 22:10:12 -0400

    second

--- beatemup/bemuplayer.gd
+++ beatemup/bemuplayer.gd
@@ -1,11 +1,21 @@
 extends CharacterBody2D
 
-@export var speed =100;
+@export var speed =200;
+@onready var anim_tree = $AnimationTree;
 func _ready() -> void:
 	pass;
 func _process(delta):
 	var dir = Input.get_vector("Left","Right","Forward","Back");
 	self.velocity=dir*speed;
+	
+	if velocity.length() >0.01:
+		anim_tree['parameters/conditions/walk']=true;
+		anim_tree['parameters/conditions/idle']=false;
+	else:
+		anim_tree['parameters/conditions/walk']=false;
+		anim_tree['parameters/conditions/idle']=true;
+		
+		
 	move_and_slide();
 	
 	pass;
