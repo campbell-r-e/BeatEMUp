0a44117e305615d7b72a4c1620cd66986d85c7c9
bsucs <122378640+bsucs@users.noreply.github.com>
2025-10-15 22:43:56 -0400

    updated

--- beatemup/bemuplayer.gd
+++ beatemup/bemuplayer.gd
@@ -1,21 +1,35 @@
 extends CharacterBody2D
 
-@export var speed =200;
-@onready var anim_tree = $AnimationTree;
+@export var speed = 200
+@onready var anim_tree = $AnimationTree
+var facing = 1
+
 func _ready() -> void:
-	pass;
+	pass
+
 func _process(delta):
-	var dir = Input.get_vector("Left","Right","Forward","Back");
-	self.velocity=dir*speed;
-	
-	if velocity.length() >0.01:
-		anim_tree['parameters/conditions/walk']=true;
-		anim_tree['parameters/conditions/idle']=false;
+	var dir = Input.get_vector("Left", "Right", "Forward", "Back")
+	self.velocity = dir * speed
+
+	if dir.x < 0 and facing == 1:
+		self.scale.x *= -1
+		facing = -1
+
+	if dir.x > 0 and facing == -1:
+		self.scale.x *= -1
+		facing = 1
+
+	if velocity.length() > 0.01:
+		anim_tree["parameters/conditions/walk"] = true
+		anim_tree["parameters/conditions/idle"] = false
 	else:
-		anim_tree['parameters/conditions/walk']=false;
-		anim_tree['parameters/conditions/idle']=true;
-		
-		
-	move_and_slide();
-	
-	pass;
+		anim_tree["parameters/conditions/walk"] = false
+		anim_tree["parameters/conditions/idle"] = true
+
+	if Input.is_action_just_pressed("attack"):
+		anim_tree["parameters/conditions/attack"] = true
+	else:
+		anim_tree['parameters/conditions/attack']=false;
+
+	move_and_slide()
+	pass
